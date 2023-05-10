d71795c328bc77c4631dfe333c2b7ae7f5207200
许剑杰 <xujianjie@ihappy.net.cn>
2023-05-10 21:46:36 +0800

    通过flowTemplate生成flow实例

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/FlowTemplate.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/FlowTemplate.java
@@ -1,16 +1,8 @@
 package com.juliet.flow.domain.model;
 
 import com.juliet.flow.common.enums.FlowTemplateStatusEnum;
-import com.juliet.flow.common.enums.NodeTypeEnum;
-import com.juliet.flow.domain.entity.FlowTemplateEntity;
-import java.util.Date;
-import java.util.Optional;
 import lombok.Data;
-import org.apache.commons.compress.utils.Lists;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.util.CollectionUtils;
 
 /**
  * @author xujianjie
@@ -29,80 +21,38 @@ public class FlowTemplate extends BaseModel {
 
     private FlowTemplateStatusEnum status;
 
-    public Flow initTouYangFlow() {
+    /**
+     * 通过模板实例化一个流程
+     */
+    public Flow toFlowInstance() {
         Flow flow = new Flow();
-        Node start = buildStartNode();
-        Node designer = buildDesignerNode();
-        Node sampleDesigner = buildSampleDesignerNode();
-        Node end = buildEndNode();
-        start.setNext(Arrays.asList(designer));
-        designer.setNext(Arrays.asList(sampleDesigner));
-        sampleDesigner.setNext(Arrays.asList(end));
-        this.node = start;
-        flow.setNode(node);
+        flow.setFlowTemplateId(this.id);
+        flow.setNode(this.node);
+        flow.setTenantId(getTenantId());
+        cleanFlowId(flow);
         return flow;
     }
 
-    private Node buildStartNode() {
-        // start节点
-        Node nodeStart = new Node();
-        nodeStart.setType(NodeTypeEnum.START);
-        return nodeStart;
+    public static void cleanFlowId(Flow flow) {
+        flow.setId(null);
+        cleanNodeId(flow.getNode());
     }
 
-    private Node buildEndNode() {
-        // start节点
-        Node nodeStart = new Node();
-        nodeStart.setType(NodeTypeEnum.END);
-        return nodeStart;
-    }
-
-    private Node buildSampleDesignerNode() {
-        Node node = new Node();
-        node.setType(NodeTypeEnum.HANDLE);
-
-        List<Post> posts = Lists.newArrayList();
-        Post post = new Post();
-        post.setPostId("sampleDesigner");
-        post.setPostName("版师");
-        posts.add(post);
-        node.setBindPosts(posts);
-
-        List<Field> fields = new ArrayList<>();
-        fields.add(Field.builder()
-                .code("name")
-                .name("类型")
-                .build());
-        Form form = new Form();
-        form.setFields(fields);
-        form.setPath("/abc/efg/aa");
-        form.setCode("12345");
-        node.setForm(form);
-        return node;
-    }
-
-
-    private Node buildDesignerNode() {
-        Node designer = new Node();
-        designer.setType(NodeTypeEnum.HANDLE);
-
-        List<Post> posts = Lists.newArrayList();
-        Post post = new Post();
-        post.setPostId("designer");
-        post.setPostName("设计师");
-        posts.add(post);
-        designer.setBindPosts(posts);
-
-        List<Field> fields = new ArrayList<>();
-        fields.add(Field.builder()
-                .code("type")
-                .name("类型")
-                .build());
-        Form formDesigner = new Form();
-        formDesigner.setFields(fields);
-        formDesigner.setPath("/abc/efg");
-        formDesigner.setCode("123");
-        designer.setForm(formDesigner);
-        return designer;
+    public static void cleanNodeId(Node node) {
+        node.setId(null);
+        if (node.getForm() != null) {
+            node.getForm().setId(null);
+            if (!CollectionUtils.isEmpty(node.getForm().getFields())) {
+                for (Field field : node.getForm().getFields()) {
+                    field.setId(null);
+                }
+            }
+        }
+        if (CollectionUtils.isEmpty(node.getNext())) {
+            return;
+        }
+        for (Node subNode : node.getNext()) {
+            cleanNodeId(subNode);
+        }
     }
 }
