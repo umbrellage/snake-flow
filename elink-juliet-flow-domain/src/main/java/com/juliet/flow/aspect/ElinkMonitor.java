96024c437edf28a1acac1d6d7851235acbb6e424
weilang.ge <weilang.ge@percent.cn>
2023-05-23 17:25:15 +0800

    code format

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/aspect/ElinkMonitor.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/aspect/ElinkMonitor.java
@@ -47,7 +47,7 @@ public class ElinkMonitor {
         Object response = point.proceed();
 
         logger.trace("Response print.uuid:{}, url:{},method:{}, body:{} response:{}",
-            data.getUuid(), data.getUrl(), data.getHttpMethod(), data.getBody(), JSON.toJSONString(response));
+            data.getUuid(), data.getUrl(), data.getHttpMethod(), JSON.toJSONString(data.getBody()), JSON.toJSONString(response));
 
         int status = attributes.getResponse() != null ? attributes.getResponse().getStatus() : 0;
 
