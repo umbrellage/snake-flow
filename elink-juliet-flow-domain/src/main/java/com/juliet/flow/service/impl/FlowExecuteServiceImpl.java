93c0ea102e75d9a701f23c87b6b003ee63f67509
许剑杰 <xujianjie@ihappy.net.cn>
2023-05-23 20:36:50 +0800

    测试多条异常流程的情况

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/service/impl/FlowExecuteServiceImpl.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/service/impl/FlowExecuteServiceImpl.java
@@ -313,17 +313,17 @@ public class FlowExecuteServiceImpl implements FlowExecuteService {
                 return;
             }
             // 判断 存在异常流程，且异常流程已全部结束
-            boolean existsAnomalyFlowsAndFlowsEnd =
-                CollectionUtils.isNotEmpty(exFlowList) && exFlowList.stream().allMatch(Flow::isFlowEnd);
-            // 判断 存在异常流程，且异常流程没有结束
-            boolean existsAnomalyFlowsAndFlowsNotEnd =
-                CollectionUtils.isNotEmpty(exFlowList) && !exFlowList.stream().allMatch(Flow::isFlowEnd);
+//            boolean existsAnomalyFlowsAndFlowsEnd =
+//                CollectionUtils.isNotEmpty(exFlowList) && exFlowList.stream().allMatch(Flow::isFlowEnd);
+//            // 判断 存在异常流程，且异常流程没有结束
+//            boolean existsAnomalyFlowsAndFlowsNotEnd =
+//                CollectionUtils.isNotEmpty(exFlowList) && !exFlowList.stream().allMatch(Flow::isFlowEnd);
             // 当节点是异常节点时
             if (node.isProcessed()) {
-                if (existsAnomalyFlowsAndFlowsNotEnd) {
-                    throw new ServiceException("已经存在异常流程正在流转中，请等待异常流程流转完成后再进行修改", StatusCode.SERVICE_ERROR.getStatus());
-                }
-                if (existsAnomalyFlowsAndFlowsEnd || CollectionUtils.isEmpty(exFlowList)) {
+//                if (existsAnomalyFlowsAndFlowsNotEnd) {
+//                    throw new ServiceException("已经存在异常流程正在流转中，请等待异常流程流转完成后再进行修改", StatusCode.SERVICE_ERROR.getStatus());
+//                }
+//                if (existsAnomalyFlowsAndFlowsEnd || CollectionUtils.isEmpty(exFlowList)) {
                     // 该节点是异常节点，要对过去的节点进行修改，需要新建一个流程处理
                     Flow subFlow = flow.subFlow();
                     subFlow.modifyNodeStatus(node);
@@ -339,7 +339,7 @@ public class FlowExecuteServiceImpl implements FlowExecuteService {
                         callback.notify(notifyDTOList);
                     }));
                     return;
-                }
+//                }
             }
             // 当节点是非异常节点时, 因为是主流程的节点，主流程不关心是否需要合并异常流程，这个操作让异常流程去做，因为异常流程在创建是肯定比主流程慢
             // 主流程只需要判断下是否存在异常流程为结束，如果存在，主流程在完成整个流程前等待异常流程合并至主流程
