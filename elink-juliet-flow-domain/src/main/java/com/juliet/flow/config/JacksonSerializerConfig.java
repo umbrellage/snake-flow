ad25dc433cf8aa7f91d961adc463176bac1f95ec
weilang.ge <weilang.ge@percent.cn>
2023-06-27 14:21:04 +0800

    修改

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/config/JacksonSerializerConfig.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/config/JacksonSerializerConfig.java
@@ -2,6 +2,10 @@ package com.juliet.flow.config;
 
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.juliet.common.core.utils.DateUtils;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +22,11 @@ public class JacksonSerializerConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> builder
+            .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateUtils.YYYY_MM_DD_HH_MM_SS)))
             // Long 会自动转换成 String
             .serializerByType(Long.class, ToStringSerializer.instance)
             .serializerByType(Long.TYPE, ToStringSerializer.instance);
     }
+
+
 }
