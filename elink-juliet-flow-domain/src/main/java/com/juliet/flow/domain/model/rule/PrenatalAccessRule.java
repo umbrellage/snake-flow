575bbb2e86667164067b2ea182971a38e4594a90
weilang.ge <weilang.ge@percent.cn>
2023-08-07 15:54:22 +0800

    添加条件分支

---
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/rule/PrenatalAccessRule.java
@@ -0,0 +1,25 @@
+package com.juliet.flow.domain.model.rule;
+
+import com.juliet.flow.domain.model.BaseRule;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+/**
+ * PrenatalAccessRule
+ *
+ * @author Geweilang
+ * @date 2023/8/7
+ */
+@Component
+public class PrenatalAccessRule extends BaseRule {
+
+    @Override
+    public String getRuleName() {
+        return "prenatal_unqualified_rule";
+    }
+
+    @Override
+    public boolean accessRule(Map<String, Object> params) {
+        return !(params != null && params.containsKey(""));
+    }
+}
