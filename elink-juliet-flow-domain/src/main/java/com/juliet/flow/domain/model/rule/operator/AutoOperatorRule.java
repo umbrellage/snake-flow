d30e3d6d4fdc9d57a35ca53cddd32d95b8c6715d
weilang.ge <umbrellage@163.com>
2023-09-07 20:48:45 +0800

    fix

---
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/rule/operator/AutoOperatorRule.java
@@ -0,0 +1,30 @@
+package com.juliet.flow.domain.model.rule.operator;
+
+import com.juliet.flow.domain.model.BaseAssignRule;
+import com.juliet.flow.domain.model.Flow;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class AutoOperatorRule extends BaseAssignRule {
+    @Override
+    public String getRuleName() {
+        return "auto_operator";
+    }
+
+    @Override
+    public Long getAssignUserId(Map<String, Object> params, Flow flow) {
+        if (params == null || params.get("autoOperator") == null) {
+            return null;
+        }
+        Object value = params.get("autoOperator");
+        if (value instanceof Long) {
+            return (Long) value;
+        }
+        if (value instanceof String) {
+            return Long.valueOf(String.valueOf(params.get("autoOperator")));
+        }
+        return (Long) params.get("autoOperator");
+    }
+}
