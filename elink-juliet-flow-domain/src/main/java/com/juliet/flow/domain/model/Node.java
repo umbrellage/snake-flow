b6bb28a7fc819fa6e03e890ede7bedff68e3041f
weilang.ge <umbrellage@163.com>
2024-03-22 16:50:26 +0800

    fix

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/Node.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/domain/model/Node.java
@@ -181,6 +181,8 @@ public class Node extends BaseModel {
             }
             SupplierDTO supplierDTO = assignRule.getAssignSupplier(params);
             if (supplierDTO != null) {
+                // todo 清空post,因为现在没有做供应商的岗位配置所以在供应商分配清楚岗位不然品牌方会收到待办
+                bindPosts = Collections.emptyList();
                 Supplier supplier = new Supplier();
                 supplier.setSupplierId(Long.valueOf(supplierDTO.getSupplierId()));
                 supplier.setSupplierType(supplierDTO.getSupplierType());
