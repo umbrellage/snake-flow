4c49e2bd61615d112205473464529d5fc29c6daa
罗刚 <luogang@ihappy.net.cn>
2024-05-30 18:33:35 +0800

    11

--- elink-juliet-flow-client/src/main/java/com/juliet/flow/client/FlowContext.java
+++ elink-juliet-flow-client/src/main/java/com/juliet/flow/client/FlowContext.java
@@ -243,7 +243,7 @@ public class FlowContext {
         dto.setReason(reason);
         dto.setFlowId(String.valueOf(nodeFieldDTO.getFlowId()));
         dto.setNodeId(String.valueOf(nodeFieldDTO.getNodeId()));
-        dto.setRollbackType(0);
+        dto.setType(0);
         AjaxResult result = julietFlowClient.rollback(dto);
         if (result == null || !Objects.equals(result.getCode(), 200)) {
             throw new ServiceException("流程回退到开始失败!" + reason);
