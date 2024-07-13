8f6ab70e3e94c2e7c472f60c00d490d83471743a
weilang.ge <weilang.ge@percent.cn>
2024-07-13 12:00:52 +0800

    fix

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/repository/impl/HistoryTaskRepositoryImpl.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/repository/impl/HistoryTaskRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.juliet.flow.repository.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.juliet.flow.client.common.NodeStatusEnum;
 import com.juliet.flow.client.dto.HistoricTaskQueryObject;
 import com.juliet.flow.client.dto.HistoryTaskInstance;
 import com.juliet.flow.dao.NodeDao;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -26,10 +28,16 @@ public class HistoryTaskRepositoryImpl implements HistoryTaskRepository {
     @Override
     public List<HistoryTaskInstance> list(HistoricTaskQueryObject queryObject) {
         List<NodeEntity> nodeEntityList = nodeDao.selectList(Wrappers.<NodeEntity>lambdaQuery()
+            .eq(queryObject.getFinished() != null && queryObject.getFinished(), NodeEntity::getStatus, NodeStatusEnum.PROCESSED.getCode())
+            .le(queryObject.getFinishedBefore() != null, NodeEntity::getFinishTime, queryObject.getFinishedBefore())
+            .ge(queryObject.getFinishedAfter() != null, NodeEntity::getFinishTime, queryObject.getFinishedAfter())
             .eq(queryObject.getTaskAssignee() != null, NodeEntity::getProcessedBy, queryObject.getTaskAssignee())
             .eq(queryObject.getTaskBpmId() != null, NodeEntity::getFlowTemplateId, queryObject.getTaskBpmId())
+            .eq(queryObject.getProcessInstanceId() != null, NodeEntity::getFlowId, queryObject.getProcessInstanceId())
+            .in(CollectionUtils.isNotEmpty(queryObject.getProcessInstanceIds()), NodeEntity::getFlowId, queryObject.getProcessInstanceIds())
         );
-        List<HistoryTaskInstanceImpl> instanceList = nodeEntityList.stream()
+
+        return nodeEntityList.stream()
             .map(node -> {
                 HistoryTaskInstanceImpl instance = new HistoryTaskInstanceImpl();
                 instance.setId(node.getId());
@@ -37,10 +45,7 @@ public class HistoryTaskRepositoryImpl implements HistoryTaskRepository {
                 instance.setTaskCreateTime(node.getActiveTime());
                 instance.setTaskEndTime(node.getFinishTime());
                 return instance;
-            })
-            .collect(Collectors.toList());
-
-        return new ArrayList<>(instanceList);
+            }).collect(Collectors.toList());
     }
 
 
