36d0150f4c08d3169c8274762b7f254cff9bd774
weilang.ge <weilang.ge@percent.cn>
2024-11-08 14:57:30 +0800

    fix

--- elink-juliet-flow-domain/src/main/java/com/juliet/flow/service/impl/FlowExecuteServiceImpl.java
+++ elink-juliet-flow-domain/src/main/java/com/juliet/flow/service/impl/FlowExecuteServiceImpl.java
@@ -472,11 +472,11 @@ public class FlowExecuteServiceImpl implements FlowExecuteService, TaskService {
 
         flowRepository.update(flow);
         // TODO: 2024/8/27 这里真的需要吗？ 主流程节点在流转时会一并将异常流程的节点流转掉, 先注释掉吧
-        List<Flow> flowList = flowRepository.listFlowByParentId(flow.getId());
-        log.info("flowAutomate subFlowIdList:{}", flowList.stream().map(Flow::getId).collect(Collectors.toList()));
-        if (CollectionUtils.isNotEmpty(flowList)) {
-            flowList.forEach(subFlow -> flowAutomate(subFlow, automateParam));
-        }
+//        List<Flow> flowList = flowRepository.listFlowByParentId(flow.getId());
+//        log.info("flowAutomate subFlowIdList:{}", flowList.stream().map(Flow::getId).collect(Collectors.toList()));
+//        if (CollectionUtils.isNotEmpty(flowList)) {
+//            flowList.forEach(subFlow -> flowAutomate(subFlow, automateParam));
+//        }
 
     }
 
